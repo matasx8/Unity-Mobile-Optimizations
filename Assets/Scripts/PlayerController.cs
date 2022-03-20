3ad2bc3aed0130dca7602329197e3e59c80f0e41
matas.tunkevicius <matas.tunkevicius@unity3d.com>
2022-03-20 20:48:52 +0200

    Some changes with movement and random stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,34 +5,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
+    [Header("Movement Settings")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float m_Speed = 1.0f;
+    [SerializeField] private float m_SideSpeed = 1.0f;
+    [SerializeField] private float m_SwipeStrengthMultiplier = 10.0f; // unused, why do I need this again?
+    [SerializeField] private float m_SwipeMovementOnX = 3.0f;
+
     private float movementX;
-    private float movementY;
-    public float speed = 1.0f;
-    public float m_SwipeStrengthMultiplier = 10.0f;
     private Vector3 m_RunAxis;
-    private float m_SwipeMovementOnX = 3.0f;
+    private bool isDoingMovementFromInput;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         m_RunAxis = new Vector3(0.0f, rb.position.y, rb.position.z);
+        isDoingMovementFromInput = false;
     }
 
     private void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        Debug.Log(movementX);
+        if (!isDoingMovementFromInput)
+        {
+            Vector2 movementVector = movementValue.Get<Vector2>();
+            movementX = movementVector.x;
+            Debug.Log(movementX);
 
-        if(movementX != 0.0f)
-            UpdateRunAxis(movementX);
+            if (movementX != 0.0f)
+                UpdateRunAxis(movementX);
+        }
     }
 
     // physics should be handled in FixedUpdate
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, m_Speed);
 
         var dif = m_RunAxis.x - transform.position.x;
 
@@ -40,13 +46,17 @@ public class PlayerController : MonoBehaviour
         if (distance > 0.0001f)
         {
             // TODO: Sometimes player starts jittering because of floating precision error, fix that
-            rb.position = new Vector3(rb.position.x + Mathf.Sign(dif) * 0.1f, rb.position.y, rb.position.z);
+            rb.position = new Vector3(rb.position.x + Mathf.Sign(dif) * m_SideSpeed, rb.position.y, rb.position.z);
+        }
+        else
+        {
+            isDoingMovementFromInput = false;
         }
     }
 
     private void UpdateRunAxis(float xMovement)
     {
-
+        isDoingMovementFromInput = true;
         float newRunXvalue = m_RunAxis.x + Mathf.Sign(xMovement) * m_SwipeMovementOnX;
         m_RunAxis = new Vector3(newRunXvalue, m_RunAxis.y, m_RunAxis.z);
     }
