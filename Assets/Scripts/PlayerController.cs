7855c25e97115e625041b7a1460be9fa28f55194
matas.tunkevicius <matas.tunkevicius@unity3d.com>
2022-03-23 00:12:28 +0200

    Added player jumping and sliding with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+// player movement is kind of buggy but gonna be good enough unless someone fixes it
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float fwdForce;
     [SerializeField] private float sideForce;
+    [SerializeField] private float jumpSpeed;
     [SerializeField] private float m_SwipeMovementOnX = 3.0f;
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] private Transform feetTransform;
     [SerializeField] private float targetFwdSpeed;
 
+
+    [Header("Animation Settings")]
+    [SerializeField] private Animator animator;
+
     private float movementX;
+    private float movementY;
     private Vector3 m_RunAxis;
     private bool isDoingMovementFromInput;
     public bool shouldContinueMoving
@@ -27,7 +33,7 @@ public class PlayerController : MonoBehaviour
     private bool IsOnGround()
     {
         // use a sphere-cast to check if we are on what we consider ground
-        return Physics.CheckSphere(feetTransform.position, 0.15f, groundLayers);
+        return Physics.CheckSphere(feetTransform.position, 0.01f, groundLayers);
     }
 
     public bool IsMoving => rb.velocity.magnitude > 0.0f;
@@ -46,6 +52,7 @@ public class PlayerController : MonoBehaviour
         {
             Vector2 movementVector = movementValue.Get<Vector2>();
             movementX = movementVector.x;
+            movementY = movementVector.y;
 #if DEBUG_MOVEMENT
             Debug.Log(movementX);
 #endif
@@ -61,70 +68,97 @@ public class PlayerController : MonoBehaviour
         // this is a TODO for someone :)
         // if someone does decide to do this please try to fix this without changing much
         // you should be able to smooth out the velocity or force somehow
-        if(shouldContinueMoving)
+
+        var playerPosToTargetedRunAxis = m_RunAxis.x - transform.position.x;
+        float strafeSpeed;
+        float fwdSpeed;
+        float epsilon = 0.2f;
+
+        string directionStr = Mathf.Sign(playerPosToTargetedRunAxis) >= 0.0f ? "isRunningRight" : "isRunningLeft";
+        string opposiuteDirStr = Mathf.Sign(playerPosToTargetedRunAxis) < 0.0f ? "isRunningRight" : "isRunningLeft";
+
+        if (Mathf.Abs(playerPosToTargetedRunAxis) > epsilon)
         {
-            var playerPosToTargetedRunAxis = m_RunAxis.x - transform.position.x;
-            float strafeSpeed;
-            float fwdSpeed;
-            float epsilon = 0.1f;
-
-            if(Mathf.Abs(playerPosToTargetedRunAxis) > epsilon)
-            {
-                strafeSpeed = Mathf.Sign(playerPosToTargetedRunAxis) * sideForce;
-            }
-            else
-            {
-                // we've reached desired run axis, do correction to stick to axis
-                rb.position = new Vector3(m_RunAxis.x, rb.position.y, rb.position.z);
-                rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
-                strafeSpeed = 0.0f;
-                isDoingMovementFromInput = false;
-            }
-
-            fwdSpeed = rb.velocity.z > targetFwdSpeed ? fwdForce : 0;
-
-            rb.AddForce(strafeSpeed * Time.deltaTime, 0.0f, fwdForce * Time.deltaTime);
+            strafeSpeed = Mathf.Sign(playerPosToTargetedRunAxis) * sideForce;
+            animator.SetBool(directionStr, true);
+            animator.SetBool(opposiuteDirStr, false);
         }
+        else
+        {
+            // we've reached desired run axis, do correction to stick to axis
+            rb.position = new Vector3(m_RunAxis.x, rb.position.y, rb.position.z);
+            rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
+            strafeSpeed = 0.0f;
+            isDoingMovementFromInput = false;
+
+            animator.SetBool(directionStr, false);
+            animator.SetBool(opposiuteDirStr, false);
+        }
+
+        fwdSpeed = rb.velocity.z > targetFwdSpeed ? fwdForce : 0;
+
+        //if(IsOnGround())
+            rb.AddForce(strafeSpeed * Time.deltaTime, 0.0f, fwdForce * Time.deltaTime);
     }
 
-    private void HandleJump()
+    private void HandleJumpOrSlide()
     {
-        // TODO?
+        if(CanAnimationBeInterrupted())
+        {
+            if(movementY > 0.0f)         // jump
+            {
+                animator.SetBool("isJumping", true);
+
+                rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+            }
+            else if(movementY < 0.0f)     // slide
+            {
+                animator.SetBool("isSliding", true);
+            }
+        }
     }
 
     private void Update()
     {
+        Upkeep();
+
+    if (shouldContinueMoving)
+    {
         HandleMovement();
+        HandleJumpOrSlide();
+
     }
 
-    // physics should be handled in FixedUpdate
-    //private void FixedUpdate()
-    //{
-    //    if (shouldContinueMoving)
-    //    {
-    //        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, m_Speed);
+#if DEBUG_MOVEMENT
+        //Debug.Log("Animator state: " + animator.GetBool("isRunningLeft").ToString() + " - " + animator.GetBool("isRunningRight").ToString());
+#endif
+    }
 
-    //        var dif = m_RunAxis.x - transform.position.x;
-
-    //        var distance = Mathf.Abs(dif);
-    //        if (distance > 0.0001f)
-    //        {
-    //            // Sometimes player starts jittering because of floating precision error
-    //            rb.position = new Vector3(rb.position.x + Mathf.Sign(dif) * m_SideSpeed, rb.position.y, rb.position.z);
-    //        }
-    //        else
-    //        {
-    //            // This should solve the precision error and stop the jittering
-    //            rb.position = new Vector3(m_RunAxis.x, rb.position.y, rb.position.z);
-    //            isDoingMovementFromInput = false;
-    //        }
-    //    }
-    //}
+    void Upkeep()
+    {
+        // can set this to false immediately because animation will stop playing when its done
+        animator.SetBool("isSliding", false);
+        animator.SetBool("isJumping", false);
+    }
 
     private void UpdateRunAxis(float xMovement)
     {
-        isDoingMovementFromInput = true;
-        float newRunXvalue = m_RunAxis.x + Mathf.Sign(xMovement) * m_SwipeMovementOnX;
-        m_RunAxis = new Vector3(newRunXvalue, m_RunAxis.y, m_RunAxis.z);
+        if (CanAnimationBeInterrupted())
+        {
+            isDoingMovementFromInput = true;
+            float newRunXvalue = m_RunAxis.x + Mathf.Sign(xMovement) * m_SwipeMovementOnX;
+            m_RunAxis = new Vector3(newRunXvalue, m_RunAxis.y, m_RunAxis.z);
+        }
+    }
+
+    private bool CanAnimationBeInterrupted()
+    {
+        bool can = IsOnGround() && !animator.GetBool("isRunningRight") && !animator.GetBool("isRunningLeft");
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("run_slide"))
+        {
+            if (!(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f))
+                return false;
+        }
+        return can;
     }
 }
