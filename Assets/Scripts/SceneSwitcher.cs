7d72ddeafb022f30f7fe02a4ba2ab43b3fed17e5
Airidas <columbusmac@Airidas.lan>
2022-04-11 17:54:05 +0300

    Added Credits back button

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public GameObject optionsScreen; 
     public void OpenScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
     }
 }
