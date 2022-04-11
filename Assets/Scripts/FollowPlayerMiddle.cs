470311517e0184f134d874708ad99937b5002c55
matas.tunkevicius <matas.tunkevicius@unity3d.com>
2022-04-11 09:08:08 +0300

    Added background demo

---
+++ Assets/Scripts/FollowPlayerMiddle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerMiddle : MonoBehaviour
+{
+
+    public Transform m_PlayerTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var transform = GetComponent<Transform>();
+        transform.position = new Vector3(transform.position.x, transform.position.y, m_PlayerTransform.position.z);
+    }
+
+}
