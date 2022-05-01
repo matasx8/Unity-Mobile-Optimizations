3f79838ff320fe1f0ba7f0c0ae4b86f627ef6de0
matas.tunkevicius <matas.tunkevicius@unity3d.com>
2022-05-01 21:06:56 +0300

    test button added

---
+++ Assets/TestManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+public class TestManager : Singleton<TestManager>
+{
+    public bool testmode = false;
+    public void OnTestButtonClick()
+    {
+        testmode = !testmode;
+        Physics.IgnoreLayerCollision(8, 9, testmode);
+    }
+}
